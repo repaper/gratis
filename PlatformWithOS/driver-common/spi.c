c5f06981b54cc003076adc10e25aac996d429157
Christopher Hall <christopherhall.hsw@gmail.com>
2014-10-17 14:57:29 +0800

    [PlatformWithOS] #17 set cs_change to zero for SAMA5D3 board
    Since there is only one SPI block in any transfer the value of the
    should not matter.
    
    Signed-off-by: Christopher Hall <christopherhall.hsw@gmail.com>

--- PlatformWithOS/driver-common/spi.c
+++ PlatformWithOS/driver-common/spi.c
@@ -104,7 +104,7 @@ void SPI_send(SPI_type *spi, const void *buffer, size_t length) {
 			.delay_usecs = 2,
 			.speed_hz = spi->bps,
 			.bits_per_word = 8,
-			.cs_change = 1
+			.cs_change = 0
 		}
 	};
 
@@ -124,7 +124,7 @@ void SPI_read(SPI_type *spi, const void *buffer, void *received, size_t length) 
 			.delay_usecs = 2,
 			.speed_hz = spi->bps,
 			.bits_per_word = 8,
-			.cs_change = 1
+			.cs_change = 0
 		}
 	};
 
